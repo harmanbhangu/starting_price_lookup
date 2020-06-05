d3f6752d496151a214b2c16a5ac8798769e5a4ad
Harmandeep Singh <harmandeep.singh3@bell.ca>
2020-06-05 16:10:07 -0400

    Get device list and get device's plan details after user selection, Added more comments for code

--- pricelookup_tool.py
+++ pricelookup_tool.py
@@ -1,7 +1,9 @@
+####### Do Imports #######
 from selenium import webdriver
 from time import sleep
 import sys
 
+####### Initialize variables & constants #######
 CHROMEDRIVER_PATH = "C:\python3\Scripts\chromedriver.exe"
 
 BELL_HOMEPAGE = "https://www.bell.ca/Mobility/Smartphones_and_mobile_internet_devices"
@@ -29,10 +31,12 @@ def welcome_user(name="Jatin"):
     print("Welcome, {} !! Please find the list of top 12 devices below:".format(name))
 
 
-
+####### Get Bell's Home Page and get list of devices #######
 def get_page(driver, page):
     driver.get(page)
 
+
+####### Util methods #######
 def maximize_window(driver):
     driver.maximize_window()
 
@@ -40,12 +44,66 @@ def maximize_window(driver):
 def wait_implicitly(driver, time=1000):
     driver.implicitly_wait(time)
 
+
 def print_chars(char="-", times=1):
     print(char * times)
 
+
+####### Print options to select from and wait for user input #######
+def show_device_list(driver):
+    get_page(driver, BELL_HOMEPAGE)
+    wait_implicitly(driver, 100)
+
+    device_dict = {}
+    device_name = {}
+
+    for i in range(0, 12):
+        var = DEVICE_ITEM_XPATH.format(str(i))
+        device = driver.find_element_by_xpath(var)
+        n = str(i + 1)
+        device_name[n] = str(device.text)
+
+        element = n + ". " + device_name[n]
+        device_dict[n] = driver.find_elements_by_xpath(LINK_TO_DEVICE.format(n))
+        print(element)
+
+    device_no = str(
+        input("\nPlease select the device number b/w 1-12 for pricing details or any other character to quit: "))
+
+    while (int(device_no) > 0 and int(device_no) < 13):
+        get_device_details(driver, device_dict, device_no, device_name[device_no])
+        driver.back()
+        sleep(3)
+        show_device_list(driver)
+
+    shutdown_application(driver)
+
+
+####### Get plan details of selected device #######
+def get_device_details(driver, device_dict, id, device_name):
+    device_dict[id][0].click()
+
+    details = driver.find_elements_by_xpath(PLAN_DETAILS)
+    wait_implicitly(driver, time=100)
+
+    print_chars("\n", 2)
+    print_chars("*", 50)
+    print("You have selected {}".format(device_name))
+    print_chars("*", 50)
+
+    print("The Plan Details are: \n")
+
+    for i in details:
+        print(i.get_attribute("innerText"))
+
+    print("\n\n")
+
+
+####### Exit driver #######
 def shutdown_application(driver):
     driver.quit()
     sys.exit(0)
+
 
 if __name__ == "__main__":
     try:
@@ -53,6 +111,7 @@ if __name__ == "__main__":
         welcome_user(name="Harman")
         get_page(driver, BELL_HOMEPAGE)
         wait_implicitly(driver, time=100)
+        show_device_list(driver)
         sleep(20)
     except Exception as e:
         raise e
